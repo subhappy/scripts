85a3cff40b72ad84977836d4433c9bac480b4eab
Lars Augensen <subhappy@users.noreply.github.com>
2024-06-09 18:00:30 +0200

    Update get-serials.sh

--- get-serials.sh
+++ get-serials.sh
@@ -1,1 +1,2 @@
+#does not yet get the serial number from MMC disks, i.e. Lenovo Yoga. This is because they simply show up as a "device"
 sudo -s eval 'lshw -class system | grep -m 2 -E "product|serial" | cut -d: -f2 && lshw -class storage | grep serial | cut -d: -f2 && lshw -class disk | grep serial | cut -d: -f2'
