744480dd6c4e39b688f025c69f30a6f66d089955
Lars Augensen <subhappy@users.noreply.github.com>
2024-06-09 18:38:53 +0200

    Update get-serials.sh
    needed to be uppercase

--- get-serials.sh
+++ get-serials.sh
@@ -1,3 +1,3 @@
 sudo -s eval 'lshw -class system | grep -m 2 -E "product|serial" | cut -d: -f2 && lshw -class storage | grep serial | cut -d: -f2 && lshw -class disk | grep serial | cut -d: -f2'
 # only required on devices with mmc-disks, i.e. lenovo yoga
-sudo udevadm info --query=property --name=/dev/mmcblk0 | grep serial | cut -d= -f2
+sudo udevadm info --query=property --name=/dev/mmcblk0 | grep SERIAL | cut -d= -f2
