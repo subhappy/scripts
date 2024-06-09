a7288ac1116e5392bad748b8ff91efd01a863708
Lars Augensen <subhappy@users.noreply.github.com>
2024-06-09 12:45:39 +0200

    Update get-serials.sh

--- get-serials.sh
+++ get-serials.sh
@@ -1,1 +1,1 @@
-sudo -s eval 'lshw -class system | grep -m 2 -E "product|serial" | cut -d: -f2 && lshw -class storage | grep serial | cut -d: -f2'
+sudo -s eval 'lshw -class system | grep -m 2 -E "product|serial" | cut -d: -f2 && lshw -class storage | grep serial | cut -d: -f2 && lshw -class disk | grep serial | cut -d: -f2'
