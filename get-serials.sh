bea045211fb608d1026a4aaefcc1f01975352d5a
Lars Augensen <subhappy@users.noreply.github.com>
2024-06-09 18:31:57 +0200

    Update get-serials.sh
    now also does Yoga

--- get-serials.sh
+++ get-serials.sh
@@ -1,2 +1,3 @@
-#does not yet get the serial number from MMC disks, i.e. Lenovo Yoga. This is because they simply show up as a "device"
 sudo -s eval 'lshw -class system | grep -m 2 -E "product|serial" | cut -d: -f2 && lshw -class storage | grep serial | cut -d: -f2 && lshw -class disk | grep serial | cut -d: -f2'
+# only required on devices with mmc-disks, i.e. lenovo yoga
+sudo udevadm info --query=property --name=/dev/mmcblk0 | grep serial | cut -d= -f2
